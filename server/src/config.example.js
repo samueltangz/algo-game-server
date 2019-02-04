3ebd6407377a22c45b02cbfa1631a966afb37768
Samuel Tang <samueltangz613@gmail.com>
2019-02-05 00:52:13 +0800

    Add example files

---
+++ server/src/config.example.js
@@ -0,0 +1,11 @@
+config = {
+  port: 3000,
+  mysql: {
+    host: 'localhost',
+    user: 'algo-dev',
+    password: 'development',
+    database: 'algo_development'
+  }
+}
+
+exports.config = config
