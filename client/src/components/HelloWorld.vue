00d329eb546e835768414a4916dc0f8692d7926b
Samuel Tang <samueltangz613@gmail.com>
2019-02-05 19:10:17 +0800

    Add websocket support

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+const io = require('socket.io-client')
+
+const socket = io('http://0.0.0.0:14937')
+
+socket.on('connect', function () {
+  console.log(socket.connected)
+  socket.on('chat message', function (message) {
+    console.log(message)
+    socket.emit('chat message', message + '!')
+  })
+})
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
