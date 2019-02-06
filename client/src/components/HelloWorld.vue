9ea774167a5e8dc113b3e828127d335d87077bef
Samuel Tang <samueltangz613@gmail.com>
2019-02-06 17:53:28 +0800

    Finishing websocket support

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -9,10 +9,22 @@ const io = require('socket.io-client')
 const socket = io('http://0.0.0.0:14937')
 
 socket.on('connect', function () {
-  console.log(socket.connected)
-  socket.on('chat message', function (message) {
+  if (Math.random() < 0.5) {
+    socket.emit('token', 'xCYZnHjSfm62W5Mg9XfPt2KCucwlfexXIwyqZNkDZ-8')
+  } else {
+    socket.emit('token', 'iAUAKT30rIJvp4lVcbfEhHaZOsIX5oITKHVSXXSsaYM')
+  }
+  socket.on('game_broadcast', function (message) {
+    console.log(`%c${message}`, 'background: #888; color: #0505AD')
+  })
+  socket.on('room_broadcast', function (message) {
+    console.log(`%c${message}`, 'background: #888; color: #0505AD')
+  })
+  socket.on('game_message', function (message) {
+    console.log(`%c${message}`, 'color: red')
+  })
+  socket.on('room_message', function (message) {
     console.log(message)
-    socket.emit('chat message', message + '!')
   })
 })
 
