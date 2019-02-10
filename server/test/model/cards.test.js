67cbe054629df1a6ee43f928531bcd0f0970cad4
Samuel Tang <samueltangz613@gmail.com>
2019-02-10 23:03:35 +0800

    Added more tests

--- server/test/model/cards.test.js
+++ server/test/model/cards.test.js
@@ -20,6 +20,7 @@ describe('model/cards.js', () => {
   describe('createCard', () => {
     const fn = cards.createCard
     test('should create a new card', async () => {
+      // Mock
       mysqlMock
         .expects('query')
         .withArgs(
@@ -27,9 +28,26 @@ describe('model/cards.js', () => {
           [ 1, cardColor.BLACK, 11, 0 ])
         .callsArgWith(2, null, { affectedRows: 1 })
 
+      // Test
       await assert.doesNotReject(async () => fn(1, cardColor.BLACK, 11, 0))
     })
+    test('should throws error when error occurs', async () => {
+      // Mock
+      mysqlMock
+        .expects('query')
+        .withArgs(
+          'INSERT INTO cards (`id`, `game_id`, `user_id`, `color`, `value`, `order`, `is_picked`) VALUES (NULL, ?, NULL, ?, ?, ?, false)',
+          [ 1, cardColor.BLACK, 11, 0 ])
+        .callsArgWith(2, new Error('the database is daydreaming'), undefined)
+
+      // Test
+      await assert.rejects(
+        async () => fn(1, cardColor.BLACK, 11, 0),
+        /^Error: the database is daydreaming$/
+      )
+    })
     test('should throws when a new card cannot be created', async () => {
+      // Mock
       mysqlMock
         .expects('query')
         .withArgs(
@@ -37,6 +55,7 @@ describe('model/cards.js', () => {
           [ 1, cardColor.BLACK, -7, 0 ])
         .callsArgWith(2, null, { affectedRows: 0 })
 
+      // Test
       await assert.rejects(
         async () => fn(1, cardColor.BLACK, -7, 0),
         /^Error: cannot create card$/
@@ -47,6 +66,7 @@ describe('model/cards.js', () => {
   describe('drawCard', () => {
     const fn = cards.drawCard
     test('should draw a card', async () => {
+      // Mock
       mysqlMock
         .expects('query')
         .withArgs(
@@ -54,9 +74,26 @@ describe('model/cards.js', () => {
           [ 17, 0, 1, 0 ])
         .callsArgWith(2, null, { changedRows: 1 })
 
+      // Test
       await assert.doesNotReject(async () => fn(1, 0, 17))
     })
+    test('should throws error when error occurs', async () => {
+      // Mock
+      mysqlMock
+        .expects('query')
+        .withArgs(
+          'UPDATE `cards` SET `user_id` = ?, `is_picked` = ? WHERE `game_id` = ? AND `order` = ? LIMIT 1',
+          [ 17, 0, 1, 0 ])
+        .callsArgWith(2, new Error('the database is daydreaming again'), undefined)
+
+      // Test
+      await assert.rejects(
+        async () => fn(1, 0, 17),
+        /^Error: the database is daydreaming again$/
+      )
+    })
     test('should throws when a new card cannot be drawn', async () => {
+      // Mock
       mysqlMock
         .expects('query')
         .withArgs(
@@ -64,6 +101,7 @@ describe('model/cards.js', () => {
           [ 17, 1, 1, 24 ])
         .callsArgWith(2, null, { changedRows: 0 })
 
+      // Test
       await assert.rejects(
         async () => fn(1, 24, 17, true),
         /^Error: cannot update card deck pointer$/
