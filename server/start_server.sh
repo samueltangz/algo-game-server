4394d13a2b698d05eb354b361cd7e37d4a2c305e
wing <poonlokwing@gmail.com>
2019-02-28 17:31:29 +0800

    forgot to change polling mysql details to env vars

--- server/start_server.sh
+++ server/start_server.sh
@@ -1,5 +1,5 @@
 cd server
-while ! (mysqladmin ping --host=mysql --user=algo-dev --password=development > /dev/null 2>&1); do
+while ! (mysqladmin ping --host=$MYSQL_HOST --user=$MYSQL_USER --password=$MYSQL_PASSWORD > /dev/null 2>&1); do
     echo "mysql not yet ready";
     sleep 3;
 done;
